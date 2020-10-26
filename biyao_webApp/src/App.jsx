3a726e202a9bd15ef66c8f167aceca997028570a
xljdknj <718285200@qq.com>
2020-10-26 18:05:24 +0800

    10.26

--- biyao_webApp/src/App.jsx
+++ biyao_webApp/src/App.jsx
@@ -1,79 +1,141 @@
-import React from 'react';//React.createElement()，用组件的地方都需要引用；
-import { Route, Redirect, Switch, withRouter } from 'react-router-dom';// React-router4 提供了导航功能的组件；安装npm install react-router-dom -D
+import React from "react"; //React.createElement()，用组件的地方都需要引用；
+import { Route, Redirect, Switch, withRouter, NavLink } from "react-router-dom"; // React-router4 提供了导航功能的组件；安装npm install react-router-dom -D
 
-import 'antd-mobile/dist/antd-mobile.css';
+import "antd-mobile/dist/antd-mobile.css";
 
+import Home from "@/views/Home";
+import List from "@/views/List";
+import Cart from "@/views/Cart";
+import User from "@/views/User";
+import Login from "@/views/Login";
+import Reg from "@/views/Reg";
+import "./App.scss";
 
-import Home from '@/views/Home';
-import List from '@/views/List';
-import Cart from '@/views/Cart';
-import User from '@/views/User';
-import Login from '@/views/Login';
-import Reg from '@/views/Reg';
-
+@withRouter
 class App extends React.Component {
     state = {
         // 配置一级路由表
-        menu: [
+        route: [
             {
-                text: '首页',
-                path: '/home',
-                name: 'home',
-                component: Home,
+                text: "首页",
+                path: "/home",
+                name: "home",
+                component: Home
             },
             {
-                text: '分类',
-                path: '/list',
-                name: 'list',
+                text: "分类",
+                path: "/list",
+                name: "list",
                 component: List
             },
             {
-                text: '购物车',
-                path: '/cart',
-                name: 'cart',
-                component: Cart,
+                text: "购物车",
+                path: "/cart",
+                name: "cart",
+                component: Cart
             },
             {
-                text: '我的',
-                path: '/user',
-                name: 'user',
-                component: User,
+                text: "我的",
+                path: "/user",
+                name: "user",
+                component: User
             },
             {
-                text: '注册',
-                path: '/reg',
-                name: 'reg',
-                component: Reg,
+                text: "注册",
+                path: "/reg",
+                name: "reg",
+                component: Reg
             },
             {
-                text: '登录',
-                path: '/login',
-                name: 'login',
-                component: Login,
-            },
-
+                text: "登录",
+                path: "/login",
+                name: "login",
+                component: Login
+            }
         ],
+
+        //底部导航栏
+        menu: [
+            {
+                text: "首页",
+                path: "/home",
+                name: "home",
+                component: Home,
+                icon: "iconfont icon-home2"
+            },
+            {
+                text: "分类",
+                path: "/list",
+                name: "list",
+                component: List,
+                icon: "iconfont icon-fenlei"
+            },
+            {
+                text: "购物车",
+                path: "/cart",
+                name: "cart",
+                component: Cart,
+                icon: "iconfont icon-cart"
+            },
+            {
+                text: "我的",
+                path: "/user",
+                name: "user",
+                component: User,
+                icon: "iconfont icon-wode"
+            }
+        ]
     };
 
-
     render() {
-        const { menu } = this.state;
-        return (
-            <div>
-                App
+        const { route, menu } = this.state;
 
+        let currPath = this.props.location.pathname.split("/")[1];
+        return (
+            <div className="tabBarWrap">
+                {/* 根据哈西判断是否显示底部导航栏 */}
+                {currPath != "details" ? (
+                    <nav>
+                        <ul className="tabbar-container">
+                            {menu.map(item => (
+                                <NavLink
+                                    to={item.path}
+                                    activeStyle={{ color: "#7f4395" }}
+                                    key={item.name}
+                                >
+                                    <li className="tabbar-item tabbar-action ">
+                                        <i className={item.icon}></i>
+                                        <div className="tabbar-itemtitle">
+                                            {item.text}
+                                        </div>
+                                    </li>
+                                </NavLink>
+                            ))}
+                        </ul>
+                    </nav>
+                ) : null}
                 <Switch>
-                    {
-                        menu.map((item) => {
-                            return <Route key={item.name} path={item.path} component={item.component} />
-                        })
-                    }
-                    <Route path='/notfound' exact render={() => { <div>404</div> }} />
-                    <Redirect from='/' to='/home' exact />
-                    <Redirect to='/notfound' />
+                    {route.map(item => {
+                        return (
+                            <Route
+                                key={item.name}
+                                path={item.path}
+                                component={item.component}
+                            />
+                        );
+                    })}
+                    <Route
+                        path="/notfound"
+                        exact
+                        render={() => {
+                            <div>404</div>;
+                        }}
+                    />
+                    <Redirect from="/" to="/home" exact />
+                    <Redirect to="/notfound" />
                 </Switch>
             </div>
-        )
+        );
     }
 }
 
